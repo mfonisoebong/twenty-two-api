dc4f0b78572b196f6e986d06946183fe9ddee2f9
mfoniso <mfonisoischris@gmail.com>
2025-07-01 12:55:33 +0100

    fixes: bug fixes on price numeric value

---
+++ database/migrations/2025_07_01_115414_change_invoice_items_price.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('invoice_items', function (Blueprint $table) {
+            $table->decimal('unit_price', 20)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('invoice_items', function (Blueprint $table) {
+            $table->decimal('unit_price')->change();
+        });
+    }
+};
